884047d8b9ad19addd2129fe91c36502236a6da0
PAFgz <pierreantoine.fagniez@gmail.com>
2021-04-29 17:58:27 +0100

    Save work

---
+++ live/src/test/java/com/pbj/sdk/testUtils/TestCoroutineRule.kt
@@ -0,0 +1,55 @@
+package com.pbj.sdk.testUtils
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.*
+import org.junit.rules.TestRule
+import org.junit.rules.TestWatcher
+import org.junit.runner.Description
+import org.junit.runners.model.Statement
+
+@ExperimentalCoroutinesApi
+class TestCoroutineRule : TestRule {
+
+    private val testCoroutineDispatcher = TestCoroutineDispatcher()
+
+    private val testCoroutineScope = TestCoroutineScope(testCoroutineDispatcher)
+
+    override fun apply(base: Statement, description: Description?) = object : Statement() {
+        @Throws(Throwable::class)
+        override fun evaluate() {
+            Dispatchers.setMain(testCoroutineDispatcher)
+
+            base.evaluate()
+
+            Dispatchers.resetMain()
+            testCoroutineScope.cleanupTestCoroutines()
+        }
+    }
+
+    fun runBlockingTest(block: suspend TestCoroutineScope.() -> Unit) =
+        testCoroutineScope.runBlockingTest { block() }
+}
+
+@ExperimentalCoroutinesApi
+fun MainCoroutineRule.runBlockingTest(block: suspend () -> Unit) =
+    this.testDispatcher.runBlockingTest {
+        block()
+    }
+
+@ExperimentalCoroutinesApi
+class MainCoroutineRule(
+    val testDispatcher: TestCoroutineDispatcher = TestCoroutineDispatcher()
+) : TestWatcher() {
+
+    override fun starting(description: Description?) {
+        super.starting(description)
+        Dispatchers.setMain(testDispatcher)
+    }
+
+    override fun finished(description: Description?) {
+        super.finished(description)
+        Dispatchers.resetMain()
+        testDispatcher.cleanupTestCoroutines()
+    }
+}
