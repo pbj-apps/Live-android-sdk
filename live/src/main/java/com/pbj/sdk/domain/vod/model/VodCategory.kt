0cf7091e6b1e671a146735e3ab876c902e015455
pierre-antoinefagniez <pierre-antoine.fagniez@realifetech.com>
2021-06-03 17:26:36 +0200

    Update gradle libraries
    Products VOD
    
    Add Product Button to Video Player
    
    Add UI visibility control

--- live/src/main/java/com/pbj/sdk/domain/vod/model/VodCategory.kt
+++ live/src/main/java/com/pbj/sdk/domain/vod/model/VodCategory.kt
@@ -3,5 +3,6 @@ package com.pbj.sdk.domain.vod.model
 data class VodCategory(
         val id: String,
         val title: String,
-        val items: List<VodItem>
+        val items: List<VodItem>,
+        val featuredItems: List<VodItem>
 )
