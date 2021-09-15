3c40fa67271e3de40202c00ddcd263c1543746ce
pierre-antoinefagniez <pierre-antoine.fagniez@realifetech.com>
2021-09-15 15:18:01 +0200

    Improve Login error management

--- live/src/main/java/com/pbj/sdk/domain/live/model/EpisodeResponse.kt
+++ live/src/main/java/com/pbj/sdk/domain/live/model/EpisodeResponse.kt
@@ -1,7 +1,7 @@
 package com.pbj.sdk.domain.live.model
 
 data class EpisodeResponse(
-    val count: Int,
+    val count: Int?,
     val next: String?,
     val previous: String?,
     val results: List<Episode>
