1871ee353382874bafb80fbcecd6ebe7826a2aa3
pierre-antoinefagniez <pierre-antoine.fagniez@realifetech.com>
2021-09-20 12:43:31 +0200

    Add documentation to UserFeature

--- live/src/main/java/com/pbj/sdk/user/UserFeature.kt
+++ live/src/main/java/com/pbj/sdk/user/UserFeature.kt
@@ -11,7 +11,7 @@ interface UserFeature {
      * @param email a string representing the email of the user
      * @param password a string representing the password related to a user account
      * @param onError callback called when an error occurred during login
-     * @param onSuccess callback called when the user logged in successfully
+     * @param onSuccess callback returning the current user. It is called when the user is logged in successfully
      */
     fun login(
         email: String,
@@ -23,7 +23,7 @@ interface UserFeature {
     /**Create a new user account
      * @param registerRequest an object containing the necessary data to create a new user account
      * @param onError callback called when an error occurred during sign up
-     * @param onSuccess callback called when the user signed up successfully
+     * @param onSuccess callback returning the current user. It is called when the user signed up successfully
      */
     fun register(
         registerRequest: RegisterRequest,
@@ -33,7 +33,7 @@ interface UserFeature {
 
     /**Get current user data
      * @param onError callback called when an error occurred
-     * @param onSuccess callback called when data have been retrieved successfully
+     * @param onSuccess callback returning the current user. It is called when data have been retrieved successfully
      */
     fun getUser(
         onError: onErrorCallBack? = null,
@@ -43,13 +43,19 @@ interface UserFeature {
 
     /**Get current user saved locally in the phone
      * @param onError callback called when an error occurred
-     * @param onSuccess callback called when data have been retrieved successfully
+     * @param onSuccess callback returning the current user. It is called when data have been retrieved successfully
      */
     fun getLocalUser(
         onError: onErrorCallBack? = null,
         onSuccess: ((User?) -> Unit)? = null
     )
 
+    /**Update current user
+     * @param firstname new firstname for the current user
+     * @param lastname new lastname for the current user
+     * @param onError callback called when an error occurred
+     * @param onSuccess callback returning the updated user. It is called when user has been updated successfully
+     */
     fun updateUser(
         firstname: String,
         lastname: String,
@@ -57,24 +63,52 @@ interface UserFeature {
         onSuccess: ((User?) -> Unit)? = null
     )
 
+    /**Update current user's profile picture
+     * @param image file representing user's new profile picture
+     * @param onError callback called when an error occurred
+     * @param onSuccess callback returning the updated user. It is called when the profile picture of the user has been updated successfully
+     */
     fun uploadProfilePicture(
         image: File,
         onError: onErrorCallBack? = null,
         onSuccess: ((User?) -> Unit)? = null
     )
 
+    /**Update current user's password
+     * @param currentPassword the current password of the user
+     * @param newPassword the new password of the user
+     * @param onError callback called when an error occurred
+     * @param onSuccess callback called when user's password has been updated successfully
+     */
     fun changePassword(
-        currentPassword: String, newPassword: String,
+        currentPassword: String,
+        newPassword: String,
         onError: onErrorCallBack? = null,
         onSuccess: (() -> Unit)? = null
     )
 
+    /**logout current user
+     * @param onError callback called when an error occurred
+     * @param onSuccess callback called when current user has been logged out successfully
+     */
     fun logout(onError: onErrorCallBack? = null, onSuccess: (() -> Unit)? = null)
 
+    /** Check if user is logged in or not
+     * @param onError callback called when an error occurred
+     * @param onSuccess callback returning true if the user is logged in or false if not
+     */
     fun isUserLoggedIn(onResult: ((Boolean) -> Unit)? = null)
 
+    /** Check if user is a guest or not
+     * @param onError callback called when an error occurred
+     * @param onSuccess callback returning true if the current user is a guest or false if not
+     */
     fun isLoggedInAsGuest(onError: onErrorCallBack? = null, onSuccess: ((Boolean?) -> Unit)? = null)
 
+    /** Check if user is a guest or not
+     * @param onError callback called when an error occurred
+     * @param onSuccess callback returning true if the current user is a guest or false if not
+     */
     fun saveIsLoggedInAsGuest(
         isGuest: Boolean,
         onError: onErrorCallBack? = null,
